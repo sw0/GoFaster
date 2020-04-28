87743e674a588903ce14e5c6741715228ee972b8
Shawn <linsw@live.cn>
2020-04-28 17:23:20 +0800

    Guid Command: now only generate a new GUID and print out

---
+++ GoFaster/Commands/GuidCommand.cs
@@ -0,0 +1,36 @@
+﻿using GoFaster.Models;
+using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFaster.Commands
+{
+    public class GuidCommand : BaseCommand
+    {
+        public string Name { get; set; } = "list";
+
+        public GuidCommand(GoFasterContext context) : base(context)
+        {
+        }
+
+        public override void Register(CommandLineApplication app)
+        {
+            new[] { "guid", "uuid" }.ToList()
+                .ForEach(name =>
+                app.Command(name, c =>
+                {
+                    c.Description = "Generate new GUID and set in clipboard";
+
+                    c.OnExecute(() =>
+                    {
+                        WriteLine($"Guid generated: {Guid.NewGuid()}");
+                        return 0;
+                    });
+                })
+            );
+        }
+    }
+}
